33a59e921ce4a1af43603a18f86e7e2e2948f5b4
P86071244 <P86071244@mail.com>
2019-04-09 04:37:00 +0800

    Use array of Function Pointer

--- main.c
+++ main.c
@@ -5,19 +5,25 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*FunPtr[])(int a,int b)={};
+
 int main()
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+ int a, b, result;
+ int function;
+ printf("Enter function you want\n");
+ printf("0: Plus, 1: Minus, 2: Multiply, 3: Divided\n");
+ FunPtr[0] = plus;
+ FunPtr[1] = minus;
+ FunPtr[2] = multiply;
+ FunPtr[3] = divided;
+ printf("EX: 1 function 1\n");
+ scanf("%d %d %d",&a,&function,&b);
+     
+ if((function >= 0) && (function<=3))
+ {
+  result=(*FunPtr[function])(a, b);
+ }
+ printf("a=%d , function=%d , b=%d, result=%d\n",a,function,b,result);
+ return 0;
 }
